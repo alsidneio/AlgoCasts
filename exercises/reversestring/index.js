3677711ce385223618a02811103204a197ac92d4
alsidneio <alsidneio@gmail.com>
2018-09-24 09:29:38 -0700

    string reveresal finished

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((reversed,char)=>{
+        return char+ reversed
+    },"")
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     let strArr = str.split('')
+//     let idx= strArr.length-1 
+//     for(i=0; i<Math.ceil    (strArr.length/2);i++){
+//         let saved = strArr[idx-i]
+//         strArr[idx-i] =strArr[i]
+//         strArr[i]=saved
+//     } 
+    
+//     let newString = strArr.join('')
+//     return newString
+
+// }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+//  }
