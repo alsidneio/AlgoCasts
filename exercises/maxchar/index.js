d704cf5472413e082ea6461ca6f2126f4ef1efee
alsidneio <alsidneio@gmail.com>
2018-09-24 14:19:57 -0700

    solution for return element with x # of elements

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//ask if case matters , if not convert to inout to lower case 
+function maxChar(str) {
+     let charMap={}
+     let max =0
+     let maxChar=''
+
+     for(letter of str){
+         if(charMap.hasOwnProperty(letter)){
+             charMap[letter]++
+         }else{
+             charMap[letter]=1
+         }
+     }
+
+     for(prop in charMap){
+         if(charMap[prop]>max){
+             max =charMap[prop]
+             maxChar=prop
+         }
+     }
+
+     return maxChar
+}
 
 module.exports = maxChar;
