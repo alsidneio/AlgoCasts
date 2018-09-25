cee44247d0935a32f81360a0af7ab05a53f1a8ab
alsidneio <alsidneio@gmail.com>
2018-09-25 01:24:54 -0700

    saving changes

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,11 +18,45 @@
 //       '####'
 
 function steps(n) {
-    let i=1
-    while(i<n+1){
-        console.log('#'.repeat(i)+' '.repeat(n-i))
-        i++
+  for (row = 0; row<n; row++){
+      let stair =''
+
+      for(column = 0; column<n; column++){
+          if(column<= row){
+              stair+='#'
+          }else{
+              stair +=" "
+          }
+      }
+    console.log(stair)
     }
 }
 
 module.exports = steps;
+
+//--------My solution-------//
+// function steps(n) {
+//     let i=1
+//     while(i<n+1){
+//         console.log('#'.repeat(i)+' '.repeat(n-i))
+//         i++
+//     }
+// }
+
+//-----Video Solution-----//
+// function steps(n) {
+//     for (row = 0; row<n; row++){
+//         let stair =''
+  
+//         for(column = 0; column<n; column++){
+//             if(column<= row){
+//                 stair+='#'
+//             }else{
+//                 stair +=" "
+//             }
+//         }
+//       console.log(stair)
+//       }
+//   }
+
+//-------Video Solution 2 recursive-------//
