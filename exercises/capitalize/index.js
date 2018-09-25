98091a722ce28f0228d9bc55a506aae3cebe4be9
alsidneio <alsidneio@gmail.com>
2018-09-25 00:35:14 -0700

    solutions 1 & 2 , 3rd solution is very complex

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,36 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const words= []
+
+    for (word of str.split(' ')){
+        words.push(word[0].toUpperCase()+word.slice(1))
+    }
+    return words.join(" ")
+}
 
 module.exports = capitalize;
+
+//--------------------------My Solution--------------------------//
+// function capitalize(str) {
+//     let capString = [] 
+//     let strArr= str.split(" ")
+//     for(word of strArr){
+//         capString.push(word.charAt(0).toUpperCase()+word.slice(1))
+//     }
+//     return capString.join(" ")
+// }
+//----------------------My solution End---------------------
+
+//-----------------------visdeo solution 1---------------------------------//
+// Thwe trick here to undserstand is that you can transform the object before the loop begins, then you will have les code 
+// function capitalize(str) {
+//     const words= []
+
+//     for (word of str.split(' ')){
+//         words.push(word[0].toUpperCase()+word.slice(1))
+//     }
+//     return words.join(" ")
+// }
+//--------------------END video solution 1----//
