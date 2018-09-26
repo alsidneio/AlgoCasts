59df5c7af4ae5d0b029cc4ab0e8a05aa34a027bb
alsidneio <alsidneio@gmail.com>
2018-09-26 02:35:16 -0700

    stack class created

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+    constructor(){
+        this.stack =[]
+    }
+
+    push(item){
+        this.stack.unshift(item)
+    }
+
+    pop(item){
+       return this.stack.shift(item)
+    }
+
+    peek(){
+        return this.stack[0]
+    }
+}
 
 module.exports = Stack;
