3e9d56d9c4063e83ba28e90dad7c470cedb015a2
alsidneio <alsidneio@gmail.com>
2018-09-28 12:58:43 -0700

    learned a bit about recursion calls

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,58 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data){
+        this.data=data;
+        this.left=null;
+        this.right=null
+
+    }
+
+    insert(data){
+        
+        //which node will it go to, left or right
+        if(data<this.data){
+        //is left node empty?   
+            if(!this.left){
+                this.left= new Node(data);
+            }else{
+                this.left.insert(data);
+            }
+       }else if(data>this.data){
+           if(!this.right){
+               this.right= new Node(data);
+           }else(this.right.insert(data))
+       }
+    }
+
+    contains(data){
+        if(this.data ===data){
+            return this;
+        }
+        
+        if(data<this.data){
+            if(!this.left){
+                return null;
+            }
+
+            if(data === this.left.data){
+                return this.left;
+            }else{
+                return this.left.contains(data);
+            }
+        }else if(data>this.data){
+            if(!this.right){
+                return null;
+            }
+
+            if(data === this.right.data){
+                return this.right;
+            }else{
+                return this.right.contains(data);
+            }
+        }
+    }
+}
 
 module.exports = Node;
