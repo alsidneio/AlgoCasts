0e8aaab7a834a12730fc5494d382a4a0840af706
alsidneio <alsidneio@gmail.com>
2018-09-28 03:17:16 -0700

    two different solutions choose one to memorize

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,42 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr= [root,'s'];
+    const counters=[0];
+    while(arr.length>1){
+        const node = arr.shift()
+        if(node ==='s'){
+            counters.push(0)
+            arr.push('s')
+        }else{
+            arr.push(...node.children)
+            counters[counters.length-1]++
+        }
+    }
+
+    return counters
+}
 
 module.exports = levelWidth;
+
+
+//------------------my solution------------// 
+// function levelWidth(root) {
+//     let width=[];
+//     let count = 0
+//     const holder = [root,'e'];
+//     while(holder.length>1){
+//         const node = holder.shift()
+//         if(node==='e'){
+//             width.push(count)
+//             count=0;
+//             holder.push('e')
+//         }else{
+//         count++
+//         holder.push(...node.children)
+//         }
+//     }
+//     width.push(count)
+
+//     return width;
